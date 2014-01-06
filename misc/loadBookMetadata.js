f829a798e3831def07d0a7cc05d6c9395bed259c
Keith Marshall <code@eleusis.co.uk>
2014-01-06 23:07:23 +0000

    Update to match new book data JSON format (list not hash) Also fixed the waiting issue after completion

--- misc/loadBookMetadata.js
+++ misc/loadBookMetadata.js
@@ -8,6 +8,7 @@ var fs = require('fs');
 var file = '../../../data/bldata/imagedirectory/book_metadata.json';
 
 var bookdata;
+var waitingCount = 0;
 fs.readFile(file, 'utf8', function (err, data) {
 	if (err) {
 		console.log('Error: ' + err);
@@ -15,10 +16,26 @@ fs.readFile(file, 'utf8', function (err, data) {
 	}
 	 
 	bookdata = JSON.parse(data);
-	for (var id in bookdata) {
-		var meta = bookdata[id];
-		meta._id = id;
+	for (var i = 0; i < bookdata.length; i++) {
+		var meta = bookdata[i];
+		meta._id = meta.identifier;
 		// add to Mongo:
-		db.bookmeta.update({"_id" : id}, meta, {"upsert" : true});
+		waitingCount++;
+		db.bookmeta.update({"_id" : meta._id}, meta, {"upsert" : true}, function() { waitingCount--;});
 	}
+	
+	// give save thread a chance to finish by waiting a second
+	console.log("Waiting to finish..");
+	waitForFinish();
 });
+
+function waitForFinish() {
+	setTimeout((function() {
+		console.log(waitingCount);
+		if (waitingCount <= 0) {
+			process.exit();
+		} else {
+			waitForFinish();
+		}
+	}), 1000);
+}
